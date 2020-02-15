7db8f580c59f4ee6ef9038c171c6e1bb1d5238cd
abdulrauf84 <abdul.rauf@live.se>
2020-02-15 13:09:42 +0100

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './App.css';
 import Nav from './components/Navbar';
 import Footer from './components/Footer'
 import ProductsScreen from './screens/ProductsScreen'
-import { Breadcrumbs, Typography } from '@material-ui/core'
+
+
+
+
 function App() {
 
   return (
 
-    <>
+    <Fragment>
       <Nav title='ShopOnline' topNavLink1='CLOTHES' TopNavLink2='SHOES' bg_color='transparent' />
       <div className="App">
         <ProductsScreen />
       </div>
       <Footer title='ShopOnline' topNavLink1='CLOTHES' TopNavLink2='SHOES' bg_color='#000000' />
-    </>
+    </Fragment>
   );
 }
 
